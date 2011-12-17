743db60aae5a8daf887e040ace5769e42971de30
zupo <zupo@afe85128-105f-4aaf-9e72-249f3def22f3>
2011-12-17 14:56:03 +0000

    fix KSS validation errors
    git-svn-id: https://niteoweb.repositoryhosting.com/svn/niteoweb_pas/trunk@27 afe85128-105f-4aaf-9e72-249f3def22f3

--- collective/listusers/browser/listusers.py
+++ collective/listusers/browser/listusers.py
@@ -42,15 +42,21 @@ class ListUsersForm(form.Form):
 class ListUsersFormWrapper(FormWrapper):
     """Subclass FormWrapper so that we can use a custom frame template."""
     index = ViewPageTemplateFile("formwrapper.pt")
-    form = ListUsersForm
 
 
 class ListUsersView(BrowserView):
     """A BrowserView to the ListUsersForm along with it's results."""
     index = ViewPageTemplateFile('listusers.pt')
 
-    def __call__(self):
+    def __init__(self, context, request):
+        """Override BrowserView's __init__ to create the ListUsersForm
+        for later use."""
+        BrowserView.__init__(self, context, request)
+        self.form_wrapper = ListUsersFormWrapper(self.context, self.request)
+        self.form_wrapper.form_instance = ListUsersForm(self.context, self.request)
 
+    def __call__(self):
+        """Main view method that handles rendering."""
         # Hide the editable border and tabs
         self.request.set('disable_border', True)
 
@@ -60,8 +66,12 @@ class ListUsersView(BrowserView):
             'attributes': self.attributes,
             'users': self.get_users(),
         }
-        self.form_wrapper = ListUsersFormWrapper(self.context, self.request)
         return self.index(**options)
+
+    def update(self):
+        """This is needed so that KSS validation from plone.app.z3cform works
+        as expected."""
+        self.form_wrapper.form_instance.update()
 
     def get_attributes(self):
         """TODO: docstring"""
