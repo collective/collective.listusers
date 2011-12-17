f3259bd339390673a925af8ac35281ab990970d5
zupo <zupo@afe85128-105f-4aaf-9e72-249f3def22f3>
2011-12-17 10:29:20 +0000

    display the form in a view
    git-svn-id: https://niteoweb.repositoryhosting.com/svn/niteoweb_pas/trunk@14 afe85128-105f-4aaf-9e72-249f3def22f3

--- collective/listusers/browser/listusers.py
+++ collective/listusers/browser/listusers.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-"""Module where the list users form lives."""
+"""Module that displays the List Users form."""
 
 from collective.listusers import ListUsersMessageFactory as _
 from collective.listusers.interfaces import IListUsersForm
 from z3c.form import button
 from z3c.form import field
 from z3c.form import form
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class ListUsersForm(form.Form):
@@ -33,12 +35,57 @@ class ListUsersForm(form.Form):
         if errors:
             self.status = self.formErrorsMessage
             return
-
+        self.status = _(u"list")
         return 'list'
 
     @button.buttonAndHandler(_(u"Reset"))
     def reset_form(self, action):
 
         # TODO: clear both form fields
-        self.status = _(u"reset.")
+        self.status = _(u"reset")
         return 'reset'
+
+
+from plone.z3cform.layout import FormWrapper
+
+
+class PortletFormView(FormWrapper):
+    """ Form view which renders z3c.forms embedded in a portlet.
+
+    Subclass FormWrapper so that we can use custom frame template. """
+
+    index = ViewPageTemplateFile("formwrapper.pt")
+
+
+class ListUsersView(BrowserView):
+    """A BrowserView to the ListUsersForm along with it's results."""
+    index = ViewPageTemplateFile('listusers.pt')
+
+    def __call__(self):
+        options = {
+            'title': self.request.get('form.widgets.title', None),
+        }
+        self.form_wrapper = self.getForm()
+        return self.index(**options)
+
+    def getForm(self):
+        """ Create a form instance.
+
+        @return: z3c.form wrapped for Plone 4 view
+        """
+
+        context = self.context.aq_inner
+
+        # returnURL = self.context.absolute_url()
+
+        # Create a compact version of the contact form
+        # (not all fields visible)
+        #form = ListUsersForm(context, self.request, returnURLHint=returnURL, full=False)
+        form = ListUsersForm(context, self.request)
+
+        # Wrap a form in Plone view
+        view = PortletFormView(context, self.request)
+        view = view.__of__(context)  # Make sure acquisition chain is respected
+        view.form_instance = form
+
+        return view
