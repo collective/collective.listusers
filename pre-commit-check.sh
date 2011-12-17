4cbc8b845db7eddb790ac6bf57e0c4218ed72034
zupo <zupo@afe85128-105f-4aaf-9e72-249f3def22f3>
2011-12-17 10:28:27 +0000

    added zptlint to buildout
    git-svn-id: https://niteoweb.repositoryhosting.com/svn/niteoweb_pas/trunk@13 afe85128-105f-4aaf-9e72-249f3def22f3

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -3,6 +3,9 @@
 echo 'Running tests'
 bin/test -s collective.listusers
 
+echo '====== Running ZPTLint ======'
+for pt in `find collective/listusers/ -name "*.pt"` ; do bin/zptlint $pt; done
+
 echo '====== Running PyFlakes ======'
 bin/pyflakes collective/listusers
 bin/pyflakes setup.py
