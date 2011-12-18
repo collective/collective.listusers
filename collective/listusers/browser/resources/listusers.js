61b18b8eed37a78d4fc1f611cc96c6ef6bcb155c
ielectric <ielectric@afe85128-105f-4aaf-9e72-249f3def22f3>
2011-12-18 20:52:45 +0000

    prettify JS
    git-svn-id: https://niteoweb.repositoryhosting.com/svn/niteoweb_pas/trunk@64 afe85128-105f-4aaf-9e72-249f3def22f3

--- collective/listusers/browser/resources/listusers.js
+++ collective/listusers/browser/resources/listusers.js
@@ -1,13 +1,10 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
     $("#example").dataTable({
-
-        "sDom": 'T<"clear">lfrtip', // where in DOM to inject TableTools controls
-        "oTableTools": {
-            "sSwfPath": portal_url + "/++resource++jquery.datatables/extras/TableTools/media/swf/copy_cvs_xls.swf",
-            aButtons: [ "copy", "csv", "xls"]
+        sDom: 'T<"clear">lfrtip', // where in DOM to inject TableTools controls
+        oTableTools: {
+            sSwfPath: portal_url + "/++resource++jquery.datatables/extras/TableTools/media/swf/copy_cvs_xls.swf",
+            aButtons: ["copy", "csv", "xls"]
         },
-
-        "sPaginationType": "full_numbers"
+        sPaginationType: "full_numbers"
     });
 });
