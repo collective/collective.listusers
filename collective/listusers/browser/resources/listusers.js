a42aeb50acf2bad3062de717fc7b72698038f8ff
Domen Kožar <domen@dev.si>
2011-12-26 18:16:15 +0100

    filter out vcard column on export

--- collective/listusers/browser/resources/listusers.js
+++ collective/listusers/browser/resources/listusers.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
+    var clmns = [];
+
+    // filter out vcard column when exporting
+    $('#example th').each(function (i, value) {
+        if ($(value).text() !== 'vcard') {
+            clmns.push(i)
+        }
+    });
+
     $("#example").dataTable({
         sDom: 'T<"clear">lfrtip', // where in DOM to inject TableTools controls
         oTableTools: {
             sSwfPath: portal_url + "/++resource++jquery.datatables/extras/TableTools/media/swf/copy_cvs_xls.swf",
-            aButtons: ["copy", "csv"]
+            aButtons: [{sExtends: "csv", mColumns: clmns}, {sExtends: "copy", mColumns: clmns}]
         },
         sPaginationType: "full_numbers"
     });
