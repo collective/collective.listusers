f9d9fc7044ff0d9ff6bf43c0dcd565f460aa121b
Alexander Pilz <pilz@syslab.com>
2012-05-07 00:17:16 +0200

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='collective.listusers',
-      version="1.1.1",
+      version="1.1.2",
       description="A search form for listing users.",
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       # Get more strings from
